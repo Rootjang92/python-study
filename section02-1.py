8202711ebdd03a9fb48737970fa536dcd490daac
Geunho Jang <39117502+Rootjang92@users.noreply.github.com>
2021-01-04 15:53:51 +0900

    print 함수 및 escape 문자

--- section02-1.py
+++ section02-1.py
@@ -31,3 +31,19 @@ print("{0} or {1} or {0}".format('Mac', 'Windows')) # 숫자 매핑
 print("{a} and {b}".format(a='You', b='Me'))
 
 print("%s's favorite number is %d or %f" % ('Kim', 5, 5.5)) # %s = 문자, %d = 정수, %f = 실수
+
+print("Test1: %5d, Price: %4.2f" %(775,6345.234))
+
+print("Test1: {0: 5d}, Price: {1: 4.2f}".format(775,2352.3424))
+
+print("Test1: {a: 5d}, Price: {b: 4.2f}".format(a=772, b=2352.1243))
+
+
+# escape
+
+print("'you'")
+print('\'you\'')
+print('"you"')
+print("""'you'""")
+print('\\you\\\n') #\n 개행문자.
+print('\t\t\ttest') # \t 들여쓰기.
