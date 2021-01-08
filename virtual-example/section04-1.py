1622db357fcf8d1d706e5a4222b19ecc9bc8b841
Geunho Jang <39117502+Rootjang92@users.noreply.github.com>
2021-01-08 17:32:24 +0900

    python type 개념 및 숫자형 및 연산자 실습

---
+++ virtual-example/section04-1.py
@@ -0,0 +1,46 @@
+# Section04-1
+
+# 데이터 타입
+
+v_str1 = "Niceman"
+v_bool = True
+v_str2 = "Goodboy"
+v_float = 10.3
+v_int = 7
+v_dict = {
+    "name": "Jang",
+    "age": 30
+}
+v_list = [3, 5, 7]
+v_tuple = 3, 5, 7
+v_set = {7,8,9}
+
+
+print(type(v_str1))
+print(type(v_bool))
+print(type(v_str2))
+print(type(v_float))
+print(type(v_int))
+print(type(v_dict))
+print(type(v_list))
+print(type(v_tuple))
+print(type(v_set))
+
+
+i1 = 39
+i2 = 939
+big_int1 = 999999999999999999999999999999
+big_int2 = 777777777777777777777777777777
+f1 = 1.234
+f2 = 3.14567
+f3 = .5
+f4 = 10.
+
+print(i1 * i2)
+print(big_int1 * big_int2)
+print(f1 ** f2)
+print(i2 // i1)
+
+result = f3 + i2
+print(result)
+print(type(result))
